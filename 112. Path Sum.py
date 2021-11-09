07d8cccf0bf57f59d3969f6da27e2e648612b305
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2021-11-09 09:24:05 -0500

    Update 112. Path Sum.py

--- 112. Path Sum.py
+++ 112. Path Sum.py
@@ -52,3 +52,33 @@ class Solution:
         return self.hasPathSum(root.left,targetSum) or self.hasPathSum(root.right,targetSum)
   
   
+Success
+Details 
+Runtime: 44 ms, faster than 69.62% of Python3 online submissions for Path Sum.
+Memory Usage: 15 MB, less than 77.61% of Python3 online submissions for Path Sum.
+ 
+ 
+ 
+ 
+ 
+ # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root:
+            return False
+        
+        track = [(root,targetSum - root.val),]
+        while track:
+            node,curr_sum = track.pop()
+            if not node.left and not node.right and curr_sum == 0:
+                return True
+            if node.right:
+                track.append((node.right, curr_sum - node.right.val))
+            if node.left:
+                track.append((node.left, curr_sum - node.left.val))
+        return False
