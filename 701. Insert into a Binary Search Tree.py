e47ca829b74ee76df0a6f2e2c9c88351e3575738
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2021-11-10 08:09:42 -0500

    Update 701. Insert into a Binary Search Tree.py

--- 701. Insert into a Binary Search Tree.py
+++ 701. Insert into a Binary Search Tree.py
@@ -50,4 +50,38 @@ Memory Usage: 16.5 MB, less than 99.65% of Python3 online submissions for Insert
             root.left = self.insertIntoBST(root.left,val)
         
         return root
+       
+       
+  Success
+Details 
+Runtime: 132 ms, faster than 86.30% of Python3 online submissions for Insert into a Binary Search Tree.
+Memory Usage: 16.8 MB, less than 27.28% of Python3 online submissions for Insert into a Binary Search Tree.
+ 
+ # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def insertIntoBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        node = root
+        while node:
+            if val > node.val:
+                if not node.right:
+                    node.right = TreeNode(val)
+                    return root
+                else:
+                    node = node.right
+            else:
+                if not node.left:
+                    node.left = TreeNode(val)
+                    return root
+                else:
+                    node = node.left
+        return TreeNode(val)
+ 
+ 
+ 
+
         
