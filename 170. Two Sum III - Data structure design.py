ca7d813bc46831a18ed4d2acc9327248f3b7eb5b
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2021-11-14 10:48:15 -0500

    Update 170. Two Sum III - Data structure design.py

--- 170. Two Sum III - Data structure design.py
+++ 170. Two Sum III - Data structure design.py
@@ -36,27 +36,41 @@ Details
 Runtime: 1144 ms, faster than 26.99% of Python3 online submissions for Two Sum III - Data structure design.
 Memory Usage: 18.3 MB, less than 74.35% of Python3 online submissions for Two Sum III - Data structure design.
   
-#   Design a data structure that accepts a stream of integers and checks if it has a pair of integers that sum up to a particular value.
 
-# Implement the TwoSum class:
 
-# TwoSum() Initializes the TwoSum object, with an empty array initially.
-# void add(int number) Adds number to the data structure.
-# boolean find(int value) Returns true if there exists any pair of numbers whose sum is equal to value, otherwise, it returns false.
+
+
+Success
+Details 
+Runtime: 1988 ms, faster than 18.47% of Python3 online submissions for Two Sum III - Data structure design.
+Memory Usage: 17.9 MB, less than 98.26% of Python3 online submissions for Two Sum III - Data structure design.
  
+ 
+ 
+ class TwoSum:
 
-# Example 1:
+    def __init__(self):
+        self.arr = []
+        
 
-# Input
-# ["TwoSum", "add", "add", "add", "find", "find"]
-# [[], [1], [3], [5], [4], [7]]
-# Output
-# [null, null, null, null, true, false]
+    def add(self, number: int) -> None:
+        self.arr.append(number)
+        
 
-# Explanation
-# TwoSum twoSum = new TwoSum();
-# twoSum.add(1);   // [] --> [1]
-# twoSum.add(3);   // [1] --> [1,3]
-# twoSum.add(5);   // [1,3] --> [1,3,5]
-# twoSum.find(4);  // 1 + 3 = 4, return true
-# twoSum.find(7);  // No two integers sum up to 7, return false
+    def find(self, value: int) -> bool:
+        newInfo = {}
+        
+        for idx,i in enumerate(self.arr):
+            targetSum = value - i
+            if targetSum in newInfo:return True
+            else:
+                newInfo[i] = i
+        return False
+        
+
+
+# Your TwoSum object will be instantiated and called as such:
+# obj = TwoSum()
+# obj.add(number)
+# param_2 = obj.find(value)
+
