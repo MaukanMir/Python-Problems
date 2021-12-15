54fabf842158168a013d2391bfa1881eab16e4ca
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2021-12-15 09:50:43 -0500

    Update 448. Find All Numbers Disappeared in an Array.py

--- 448. Find All Numbers Disappeared in an Array.py
+++ 448. Find All Numbers Disappeared in an Array.py
@@ -41,3 +41,25 @@ class Solution:
             if i not in keys: seen.append(i)
         
         return seen
+      
+      
+      
+#  Success
+# Details 
+# Runtime: 348 ms, faster than 64.21% of Python3 online submissions for Find All Numbers Disappeared in an Array.
+# Memory Usage: 23.2 MB, less than 46.86% of Python3 online submissions for Find All Numbers Disappeared in an Array.
+  
+  
+  class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        
+        keys = Counter(nums)
+        
+        seen = []
+        for i in range(1, len(nums)+1):
+            if i not in keys: seen.append(i)
+        
+        return seen
+  
+  
+
