cc70661c4312672947e357222f6ea3f53e94741c
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-01-13 14:56:20 -0500

    Update Phone Number Mnemoinics.py

--- Phone Number Mnemoinics.py
+++ Phone Number Mnemoinics.py
@@ -30,3 +30,37 @@ def phoneNumberMnemonics(phoneNumber):
 	
 	backtrack(0,"")
 	return seen
+
+
+# Solution 2:
+	
+	def phoneNumberMnemonics(phoneNumber):
+	currentNumber = ["0"] * len(phoneNumber)
+	found = []
+	
+	phoneHelper(0,phoneNumber,currentNumber,found)
+	return found
+
+def phoneHelper(idx,phoneNumber,currentNumber,found):
+	digits ={
+		"0": ["0"],
+		"1":["1"],
+		"2":["a","b","c"],
+		"3":["d","e","f"],
+		"4":["g","h","i"],
+		"5":["j","k","l"],
+		"6":["m","n","o"],
+		"7":["p","q","r","s"],
+		"8":["t","u","v"],
+		"9":["w","x","y","z"],
+	}
+	if idx == len(phoneNumber):
+		got = "".join(currentNumber)
+		found.append(got)
+	else:
+		digit = phoneNumber[idx]
+		letters = digits[digit]
+		
+		for letter in letters:
+			currentNumber[idx] = letter
+			phoneHelper(idx+1, phoneNumber,currentNumber,found)
