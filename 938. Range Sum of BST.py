928d174e35ccfedc66002d62efa6166b54ce05ec
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-01-14 22:48:49 -0500

    Create 938. Range Sum of BST.py

---
+++ 938. Range Sum of BST.py
@@ -0,0 +1,57 @@
+# Given the root node of a binary search tree and two integers low and high, return the sum of values of all nodes with a value in the inclusive range [low, high].
+
+ 
+
+# Example 1:
+
+
+# Input: root = [10,5,15,3,7,null,18], low = 7, high = 15
+# Output: 32
+# Explanation: Nodes 7, 10, and 15 are in the range [7, 15]. 7 + 10 + 15 = 32.
+# Example 2:
+
+
+# Input: root = [10,5,15,3,7,13,18,1,null,6], low = 6, high = 10
+# Output: 23
+# Explanation: Nodes 6, 7, and 10 are in the range [6, 10]. 6 + 7 + 10 = 23.
+ 
+
+# Constraints:
+
+# The number of nodes in the tree is in the range [1, 2 * 104].
+# 1 <= Node.val <= 105
+# 1 <= low <= high <= 105
+# All Node.val are unique.
+
+Success
+Details 
+Runtime: 208 ms, faster than 76.81% of Python3 online submissions for Range Sum of BST.
+Memory Usage: 22.2 MB, less than 85.47% of Python3 online submissions for Range Sum of BST.
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        
+        if not root: return 0
+        
+        total =0
+        stack = [root]
+        
+        while stack:
+            child =  stack.pop()
+            if child:
+                if low <= child.val <=high: total += child.val
+                if child.val >low:
+                    stack.append(child.left)
+                if child.val <high:
+                    stack.append(child.right)
+                        
+                    
+        
+        return total
