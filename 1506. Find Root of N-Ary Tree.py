2ad33131bab8a728d1f9bc852252d8f7eb386f6f
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-01-19 22:08:07 -0500

    Update 1506. Find Root of N-Ary Tree.py

--- 1506. Find Root of N-Ary Tree.py
+++ 1506. Find Root of N-Ary Tree.py
@@ -75,3 +75,31 @@ class Solution:
         for node in tree:
             if node.val not in seen:
                 return node
+Success
+Details 
+Runtime: 124 ms, faster than 100.00% of Python3 online submissions for Find Root of N-Ary Tree.
+Memory Usage: 24.7 MB, less than 85.59% of Python3 online submissions for Find Root of N-Ary Tree.
+ 
+ """
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children if children is not None else []
+"""
+
+class Solution:
+    def findRoot(self, tree: List['Node']) -> 'Node':
+        
+        total =0
+        
+        for node in tree:
+            total += node.val
+            
+            for child in node.children:
+                total -= child.val
+        
+        for node in tree:
+            if node.val == total:
+                return node
+
