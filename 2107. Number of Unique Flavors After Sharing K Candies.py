33b0b21cb0d6e0e3b4cfd620ca8e3773bc35ada0
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-02-02 12:12:49 -0500

    Create 2107. Number of Unique Flavors After Sharing K Candies.py

---
+++ 2107. Number of Unique Flavors After Sharing K Candies.py
@@ -0,0 +1,71 @@
+You are given a 0-indexed integer array candies, where candies[i] represents the flavor of the ith candy. Your mom wants you to share these candies with your little sister by giving her k consecutive candies, but you want to keep as many flavors of candies as possible.
+
+Return the maximum number of unique flavors of candy you can keep after sharing with your sister.
+
+ 
+
+Example 1:
+
+Input: candies = [1,2,2,3,4,3], k = 3
+Output: 3
+Explanation: 
+Give the candies in the range [1, 3] (inclusive) with flavors [2,2,3].
+You can eat candies with flavors [1,4,3].
+There are 3 unique flavors, so return 3.
+Example 2:
+
+Input: candies = [2,2,2,2,3,3], k = 2
+Output: 2
+Explanation: 
+Give the candies in the range [3, 4] (inclusive) with flavors [2,3].
+You can eat candies with flavors [2,2,2,3].
+There are 2 unique flavors, so return 2.
+Note that you can also share the candies with flavors [2,2] and eat the candies with flavors [2,2,3,3].
+Example 3:
+
+Input: candies = [2,4,5], k = 0
+Output: 3
+Explanation: 
+You do not have to give any candies.
+You can eat the candies with flavors [2,4,5].
+There are 3 unique flavors, so return 3.
+ 
+
+Constraints:
+
+1 <= candies.length <= 105
+1 <= candies[i] <= 105
+0 <= k <= candies.length
+
+Success
+Details 
+Runtime: 544 ms, faster than 91.53% of Python3 online submissions for Number of Unique Flavors After Sharing K Candies.
+Memory Usage: 31.4 MB, less than 27.42% of Python3 online submissions for Number of Unique Flavors After Sharing K Candies.
+
+
+class Solution:
+    def shareCandies(self, candies: List[int], k: int) -> int:
+        seen = {}
+        
+        for key in candies[k:]:
+            if key in seen:
+                seen[key] +=1
+            else:
+                seen[key] =1
+                
+        
+        size = len(seen)
+        
+        for i in range(0,len(candies)-k):
+            if candies[i] in seen:
+                seen[candies[i]] +=1
+            else:
+                seen[candies[i]] =1
+        
+            seen[candies[i+k]] -=1
+            
+            if seen[candies[i+k]] ==0:
+                del seen[candies[i+k]]
+            
+            size = max(size, len(seen))
+        return size
