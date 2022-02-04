9f9200a0cdddb5dd73c0b4f17234a7ea7f71f078
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-02-04 10:19:46 -0500

    Update 5. Longest Palindromic Substring.py

--- 5. Longest Palindromic Substring.py
+++ 5. Longest Palindromic Substring.py
@@ -51,3 +51,32 @@ class Solution:
                 l-=1
                 r+=1
         return res
+       
+       
+
+Success
+Details 
+Runtime: 960 ms, faster than 79.05% of Python3 online submissions for Longest Palindromic Substring.
+Memory Usage: 14 MB, less than 91.03% of Python3 online submissions for Longest Palindromic Substring.
+ 
+ 
+ class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        currentLongest = [0,1]
+        
+        for i in range(1,len(s)):
+            odd = find(s, i-1, i+1)
+            even = find(s,i-1, i)
+            longest = max(odd, even, key = lambda x: x[1]- x[0])
+            currentLongest = max(longest, currentLongest, key = lambda x: x[1]- x[0])
+        
+        return s[currentLongest[0]: currentLongest[1]]
+
+def find(s, left, right):
+    while left >=0 and right < len(s):
+        if s[left] != s[right]:
+            break
+        
+        left -=1
+        right +=1
+    return [left+1, right]
