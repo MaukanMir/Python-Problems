a2d72ee1bd093954e6f8e507782cef9585272ff4
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-02-04 07:44:59 -0500

    Create 2000. Reverse Prefix of Word.py

---
+++ 2000. Reverse Prefix of Word.py
@@ -0,0 +1,39 @@
+Given a 0-indexed string word and a character ch, reverse the segment of word that starts at index 0 and ends at the index of the first occurrence of ch (inclusive). If the character ch does not exist in word, do nothing.
+
+For example, if word = "abcdefd" and ch = "d", then you should reverse the segment that starts at 0 and ends at 3 (inclusive). The resulting string will be "dcbaefd".
+Return the resulting string.
+
+ 
+
+Example 1:
+
+Input: word = "abcdefd", ch = "d"
+Output: "dcbaefd"
+Explanation: The first occurrence of "d" is at index 3. 
+Reverse the part of word from 0 to 3 (inclusive), the resulting string is "dcbaefd".
+Example 2:
+
+Input: word = "xyxzxe", ch = "z"
+Output: "zxyxxe"
+Explanation: The first and only occurrence of "z" is at index 3.
+Reverse the part of word from 0 to 3 (inclusive), the resulting string is "zxyxxe".
+Example 3:
+
+Input: word = "abcd", ch = "z"
+Output: "abcd"
+Explanation: "z" does not exist in word.
+You should not do any reverse operation, the resulting string is "abcd".
+
+
+Success
+Details 
+Runtime: 50 ms, faster than 25.53% of Python3 online submissions for Reverse Prefix of Word.
+Memory Usage: 13.8 MB, less than 95.33% of Python3 online submissions for Reverse Prefix of Word.
+
+
+class Solution:
+    def reversePrefix(self, word: str, ch: str) -> str:
+        if ch not in word:
+            return word
+        start = word.index(ch)
+        return word[:start+1][::-1] + word[start+1:]
