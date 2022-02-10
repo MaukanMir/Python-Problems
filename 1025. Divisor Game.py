438662595062a70d3f364fb6b11a5d28ea3e9271
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-02-10 07:52:01 -0500

    Create 1025. Divisor Game.py

---
+++ 1025. Divisor Game.py
@@ -0,0 +1,36 @@
+Alice and Bob take turns playing a game, with Alice starting first.
+
+Initially, there is a number n on the chalkboard. On each player's turn, that player makes a move consisting of:
+
+Choosing any x with 0 < x < n and n % x == 0.
+Replacing the number n on the chalkboard with n - x.
+Also, if a player cannot make a move, they lose the game.
+
+Return true if and only if Alice wins the game, assuming both players play optimally.
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: true
+Explanation: Alice chooses 1, and Bob has no more moves.
+Example 2:
+
+Input: n = 3
+Output: false
+Explanation: Alice chooses 1, Bob chooses 1, and Alice has no more moves.
+
+
+Success
+Details 
+Runtime: 28 ms, faster than 92.91% of Python3 online submissions for Divisor Game.
+Memory Usage: 13.9 MB, less than 90.80% of Python3 online submissions for Divisor Game.
+  
+  
+  class Solution:
+    def divisorGame(self, n: int) -> bool:
+        return n % 2 ==0
+        
+  
+  
