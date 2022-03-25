e47fc284c8bf5dfc71c6aa414423b8c9fdea9d23
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-03-25 08:31:50 -0400

    Update 938. Range Sum of BST.py

--- 938. Range Sum of BST.py
+++ 938. Range Sum of BST.py
@@ -55,3 +55,21 @@ class Solution:
                     
         
         return total
+  
+  class Solution:
+    def rangeSumBST(self, root: Optional[TreeNode], low: int, high: int) -> int:
+        
+        stack, total =[root], 0
+        
+        while stack:
+            node = stack.pop()
+            
+            if node:
+                if node.val >= low and node.val <= high:
+                    total += node.val
+            
+                if node.left:
+                    stack.append(node.left)
+                if node.right:
+                    stack.append(node.right)
+        return total
