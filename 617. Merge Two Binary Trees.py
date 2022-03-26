b28108ee6de79921d423e97c8f458dd01956f993
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-03-26 12:22:45 -0400

    Update 617. Merge Two Binary Trees.py

--- 617. Merge Two Binary Trees.py
+++ 617. Merge Two Binary Trees.py
@@ -73,4 +73,40 @@ class Solution:
         root.right = self.mergeTrees(root1.right if root1 else None, root2.right if root2 else None)
         
         return root
+    
+    # Success
+# Details 
+# Runtime: 94 ms, faster than 81.20% of Python3 online submissions for Merge Two Binary Trees.
+# Memory Usage: 15.4 MB, less than 81.35% of Python3 online submissions for Merge Two Binary Trees.
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        if root1 is None:
+            return root2
         
+        stack = [[root1,root2]]
+        
+        while stack:
+            t1, t2 = stack.pop()
+            
+            if t1 is None or t2 is None:continue
+
+            t1.val += t2.val
+
+            if t1.left is None:
+                t1.left = t2.left
+            else:stack.append([t1.left,t2.left])
+            
+            if t1.right is None:
+                t1.right = t2.right
+            else:
+                stack.append([t1.right, t2.right])
+        
+        return root1
