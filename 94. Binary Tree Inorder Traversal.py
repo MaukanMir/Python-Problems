73eba5e09c7ffd2e944c1c65d7bc0210affafb41
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-03-29 09:06:43 -0400

    Update 94. Binary Tree Inorder Traversal.py

--- 94. Binary Tree Inorder Traversal.py
+++ 94. Binary Tree Inorder Traversal.py
@@ -43,3 +43,33 @@ class Solution:
         inorder(root)
         
         return result
+
+       
+Success
+Details 
+Runtime: 42 ms, faster than 56.37% of Python3 online submissions for Binary Tree Inorder Traversal.
+Memory Usage: 13.8 MB, less than 97.14% of Python3 online submissions for Binary Tree Inorder Traversal.
+ 
+ 
+ 
+ # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        
+        stack, result = [], []
+        
+        while stack or root:
+            if root:
+                stack.append(root)
+                root = root.left
+            else:
+                root = stack.pop()
+                result.append(root.val)
+                root = root.right
+        return result
+                
