465f88fa739b15590c96d4e1aadf60b5da4c300c
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-04-01 14:14:13 -0400

    Create 144. Binary Tree Preorder Traversal.py

---
+++ 144. Binary Tree Preorder Traversal.py
@@ -0,0 +1,52 @@
+Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+
+
+Success
+Details 
+Runtime: 76 ms, faster than 5.38% of Python3 online submissions for Binary Tree Preorder Traversal.
+Memory Usage: 13.8 MB, less than 63.16% of Python3 online submissions for Binary Tree Preorder Traversal.
+
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        
+        result = []
+        
+        def preOrder(node):
+            if node:
+                result.append(node.val)
+                preOrder(node.left)
+                preOrder(node.right)
+        
+        
+        preOrder(root)
+        return result
