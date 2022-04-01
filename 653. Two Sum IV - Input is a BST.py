a066e925ab198e2ac15c818e2973dcd6b60b71be
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-04-01 15:39:58 -0400

    Update 653. Two Sum IV - Input is a BST.py

--- 653. Two Sum IV - Input is a BST.py
+++ 653. Two Sum IV - Input is a BST.py
@@ -62,3 +62,34 @@ class Solution:
             
             return findValue(root.left,k,container) or findValue(root.right,k, container)
         return findValue(root,k,container)
+       
+ Success
+Details 
+Runtime: 130 ms, faster than 43.88% of Python3 online submissions for Two Sum IV - Input is a BST.
+Memory Usage: 16.4 MB, less than 75.24% of Python3 online submissions for Two Sum IV - Input is a BST.
+ 
+ 
+ Given the root of a Binary Search Tree and a target number k, return true if there exist two elements in the BST such that their sum is equal to the given target.
+
+ 
+
+Example 1:
+
+
+Input: root = [5,3,6,2,4,null,7], k = 9
+Output: true
+Example 2:
+
+
+Input: root = [5,3,6,2,4,null,7], k = 28
+Output: false
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 104].
+-104 <= Node.val <= 104
+root is guaranteed to be a valid binary search tree.
+-105 <= k <= 105
+
+
