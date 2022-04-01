28a941186a4dd7c4f3c4f60391ee80c736f6c6bc
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-04-01 14:22:16 -0400

    Update 144. Binary Tree Preorder Traversal.py

--- 144. Binary Tree Preorder Traversal.py
+++ 144. Binary Tree Preorder Traversal.py
@@ -50,3 +50,32 @@ class Solution:
         
         preOrder(root)
         return result
+
+       
+       
+Success
+Details 
+Runtime: 46 ms, faster than 44.67% of Python3 online submissions for Binary Tree Preorder Traversal.
+Memory Usage: 13.9 MB, less than 63.16% of Python3 online submissions for Binary Tree Preorder Traversal.
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        
+        if not root:
+            return []
+    
+        result,stack = [], [root]
+        
+        while stack:
+            node = stack.pop()
+            
+            if node:
+                result.append(node.val)
+            if node.right: stack.append(node.right)
+            if node.left: stack.append(node.left)
+        return result
