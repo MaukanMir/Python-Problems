e66a0b7e53334490fdee1187c21006f93d6efa1d
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-04-09 10:07:11 -0400

    Update 103. Binary Tree Zigzag Level Order Traversal.py

--- 103. Binary Tree Zigzag Level Order Traversal.py
+++ 103. Binary Tree Zigzag Level Order Traversal.py
@@ -64,6 +64,40 @@ class Solution:
                 res.append(keys[k][::-1])
         return res
 
-       
+Success
+Details 
+Runtime: 38 ms, faster than 73.82% of Python3 online submissions for Binary Tree Zigzag Level Order Traversal.
+Memory Usage: 14.1 MB, less than 60.90% of Python3 online submissions for Binary Tree Zigzag Level Order Traversal.
+ 
+ 
+ 
+ # Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def zigzagLevelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        
+        if not root: return []
+        stack, keys = deque([[root,0]]), defaultdict(list)
+        
+        while stack:
+            node, lvl = stack.popleft()
+
+            if node.left:
+                stack.append([node.left, lvl+1])
+            if node.right:
+                stack.append([node.right, lvl+1])
+            
+            if node and lvl not in keys:
+                keys[lvl] = [node.val]
+            elif node and lvl in keys:
+                keys[lvl].append(node.val)
+
+        
+        return [ keys[j] if i %2 ==0 else keys[j][::-1] for i,j in enumerate(keys) ]
+      
        
      
