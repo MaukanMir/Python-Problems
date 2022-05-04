ac871284dcaf9c651fdf92293c764f74ae3f136a
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-05-04 07:50:14 -0400

    Update 23. Merge k Sorted Lists.py

--- 23. Merge k Sorted Lists.py
+++ 23. Merge k Sorted Lists.py
@@ -68,3 +68,45 @@ class Solution:
         tail.next = l1 if l1 else l2
         
         return dummy.next
+
+       
+       
+ Success
+Details 
+Runtime: 114 ms, faster than 79.33% of Python3 online submissions for Merge k Sorted Lists.
+Memory Usage: 17.6 MB, less than 76.39% of Python3 online submissions for Merge k Sorted Lists.
+ 
+ # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        
+        if not lists or len(lists) ==0:
+            return None
+        
+        while len(lists) >1:
+            merged = []
+            for i in range(0,len(lists),2):
+                l1, l2 = lists[i], lists[i+1] if i+1 < len(lists) else None
+                
+                merged.append(self.merge(l1,l2))
+            lists = merged
+        return lists[0]
+    
+    def merge(self,l1,l2):
+        dummy = ListNode(0)
+        tail = dummy
+        
+        while l1 and l2:
+            if l1.val < l2.val:
+                tail.next,l1 = l1, l1.next
+            else:
+                tail.next,l2 = l2, l2.next
+            tail = tail.next
+        
+        tail.next = l1 if l1 else l2
+        
+        return dummy.next
