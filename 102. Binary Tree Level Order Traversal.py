257f254d2bb57466b329ce6b5452146d60b99384
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-05-11 08:58:51 -0400

    Update 102. Binary Tree Level Order Traversal.py

--- 102. Binary Tree Level Order Traversal.py
+++ 102. Binary Tree Level Order Traversal.py
@@ -56,3 +56,35 @@ class Solution:
                 res[lvl].append(node.val)
 
         return res.values()
+
+       
+Success
+Details 
+Runtime: 46 ms, faster than 55.13% of Python3 online submissions for Binary Tree Level Order Traversal.
+Memory Usage: 15 MB, less than 9.21% of Python3 online submissions for Binary Tree Level Order Traversal.
+
+       
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        
+        levels = []
+        if not root:
+            return levels
+        
+        def bfs(root,lvl):
+            if len(levels) == lvl:
+                levels.append([])
+            
+            levels[lvl].append(root.val)
+            if root.left:
+                bfs(root.left,lvl+1)
+            if root.right:
+                bfs(root.right,lvl+1)
+        bfs(root,0)
+        return levels
