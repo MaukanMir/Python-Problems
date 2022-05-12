a1c1a237e00384b1355ec362ca5b07d3945a2783
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-05-12 12:11:43 -0400

    Update 1448. Count Good Nodes in Binary Tree.py

--- 1448. Count Good Nodes in Binary Tree.py
+++ 1448. Count Good Nodes in Binary Tree.py
@@ -65,3 +65,30 @@ class Solution:
                      
                 
         return total
+       
+Success
+Details 
+Runtime: 467 ms, faster than 12.65% of Python3 online submissions for Count Good Nodes in Binary Tree.
+Memory Usage: 32.5 MB, less than 87.77% of Python3 online submissions for Count Good Nodes in Binary Tree.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def goodNodes(self, root: TreeNode) -> int:
+        
+        def dfs(node,maxVal):
+            if not node:
+                return 0
+            
+            res = 1 if node.val >= maxVal else 0
+            maxVal = max(maxVal,node.val)
+            res+= dfs(node.left,maxVal)
+            res += dfs(node.right, maxVal)
+            
+            return res
+        
+        return dfs(root,root.val)
