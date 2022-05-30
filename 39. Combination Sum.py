ec74cfcd602c724e15119780c24d598a7b6294f9
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-05-30 11:02:10 -0400

    Update 39. Combination Sum.py

--- 39. Combination Sum.py
+++ 39. Combination Sum.py
@@ -58,3 +58,30 @@ class Solution:
         backtrack(target,[],0)
         
         return values
+      
+      
+      Success
+Details 
+Runtime: 197 ms, faster than 15.27% of Python3 online submissions for Combination Sum.
+Memory Usage: 13.9 MB, less than 91.27% of Python3 online submissions for Combination Sum.
+
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        
+        res = []
+        
+        def dfs(i, curr, total):
+            if total == target:
+                res.append(curr[:])
+                return 
+            if i >= len(candidates) or total > target:
+                return 
+            
+            curr.append(candidates[i])
+            dfs(i, curr, total+ candidates[i])
+            curr.pop()
+            dfs(i+1, curr, total)
+        
+        dfs(0,[], 0)
+        return res
