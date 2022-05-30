d8150f2729c656f12989b6b7cfc11fcded6a7521
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-05-30 11:49:17 -0400

    Update 46. Permutations.py

--- 46. Permutations.py
+++ 46. Permutations.py
@@ -37,3 +37,28 @@ class Solution:
         n = len(nums)
         backtrack()
         return values
+
+       
+       
+Success
+Details 
+Runtime: 42 ms, faster than 85.83% of Python3 online submissions for Permutations.
+Memory Usage: 14 MB, less than 58.46% of Python3 online submissions for Permutations.
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        
+        res = []
+        
+        if len(nums) ==1:
+            return [nums[:]]
+        
+        for i in range(len(nums)):
+            n = nums.pop(0)
+            
+            perms = self.permute(nums)
+            for perm in perms:
+                perm.append(n)
+            res.extend(perms)
+            nums.append(n)
+            
+        return res
