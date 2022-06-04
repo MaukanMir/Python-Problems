7d4695ba234e2bb8ae7985265b66928b12e93242
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-06-04 19:55:42 -0400

    Update 994. Rotting Oranges.py

--- 994. Rotting Oranges.py
+++ 994. Rotting Oranges.py
@@ -68,3 +68,39 @@ class Solution:
                             stack.append((rR,cC))
         
         return minutes if fresh ==0 else -1
+       
+
+Success
+Details 
+Runtime: 54 ms, faster than 87.06% of Python3 online submissions for Rotting Oranges.
+Memory Usage: 13.9 MB, less than 46.21% of Python3 online submissions for Rotting Oranges.
+ class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        ROWS, COLS = len(grid), len(grid[0])
+        fresh,time = 0,0
+        q = deque()
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] ==2:
+                    q.append([r,c])
+                if grid[r][c]==1:
+                    fresh+=1
+        
+        directions =[[1,0], [-1,0], [0,-1], [0,1]]
+        while q and fresh >0:
+            for i in range(len(q)):
+                r,c = q.popleft()
+                for dr,dc in directions:
+                    row, col = dr+r, dc + c
+                    
+                    if row <0 or col <0 or col== COLS or row == ROWS or grid[row][col] !=1:
+                        continue
+                    grid[row][col] = 2
+                    q.append([row,col])
+                    fresh -=1
+            
+            time +=1
+        
+        
+        return time if fresh == 0 else -1
