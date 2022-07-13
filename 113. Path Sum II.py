16a894bee209c8f24dd873cf6142f2606be3820c
MaukanMir <72883238+MaukanMir@users.noreply.github.com>
2022-07-13 17:45:00 -0400

    Update 113. Path Sum II.py

--- 113. Path Sum II.py
+++ 113. Path Sum II.py
@@ -60,3 +60,32 @@ class Solution:
         
         dfs()
         return result
+
+Success
+Details 
+Runtime: 61 ms, faster than 65.75% of Python3 online submissions for Path Sum II.
+Memory Usage: 19.2 MB, less than 28.62% of Python3 online submissions for Path Sum II.
+  
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        
+        res = []
+        def dfs(root,total, totalSum):
+            if not root:
+                return 0
+            if not root.left and not root.right:
+                if targetSum == totalSum + root.val:
+                    res.append(total + [root.val])
+                return total
+        
+            dfs(root.left, total + [root.val],totalSum + root.val )
+            dfs(root.right, total + [root.val],totalSum + root.val)
+        
+        dfs(root,[],0)
+        return res
